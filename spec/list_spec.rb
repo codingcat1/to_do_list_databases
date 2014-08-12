9e496bdfa7d27fc9d9e81deabf807306143d435c
Joshua Storey & Catherine Tran <student@epicodus.com>
2014-08-12 11:39:44 -0700

    create list class and make specs pass to create functionality to initialize list with a name and return the name.

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,3 +1,26 @@
 require 'rspec'
 require 'list'
+require 'pg'
+
+DB = PG.connect(:dbname => 'to_do_list_test')
+
+Rspec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM lists *;")
+  end
+end
+
+describe List do
+
+  it "is initialized with a name" do
+    list = List.new('Some List')
+    expect(list).to be_an_instance_of List
+  end
+
+  it 'tells you its name' do
+    list = List.new('Some List')
+    expect(list.name).to eq 'Some List'
+  end
+
+end
 
